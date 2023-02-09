613a34d49f5ad2368ddca62801fe88f95e212cc3
david lutta <davidlutta2010@gmail.com>
2023-02-09 14:59:43 +0300

    Update Reasme

--- Security System/security alarm/src/main.cpp
+++ Security System/security alarm/src/main.cpp
@@ -1,11 +1,45 @@
-#include <Arduino.h>
+#include<Arduino.h>
+
+// defines pins numbers
+const int trigPin = 9;
+const int echoPin = 10;
+const int led = 12;
+// defines variables
+long duration;
+int distance;
+int threshold = 0; // set threshold.
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println("Hello World");
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(led, OUTPUT);
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.begin(9600); // Starts the serial communication
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  // if the distance measured is greater than or equal to the threshold set then we turn on the LED for 5 seconds
+  if(distance >= threshold)
+  {
+    digitalWrite(led, HIGH);
+    delay(5000);
+    digitalWrite(led, LOW);
+  }
 }
